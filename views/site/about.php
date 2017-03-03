f284d10d6fd1f9b8c654d36c6d06bebb934a291f
Edward Chernyavsky <e.chernyavsky@itransition.com>
2017-03-03 13:26:55 +0300

    "About" page

--- views/site/about.php
+++ views/site/about.php
@@ -7,12 +7,49 @@ use yii\helpers\Html;
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
+<div class="page-container">
+    <div class="page-content">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1 class="text-uppercase">О компании</h1>
+            </div>
+        </div>
+        <hr/>
+        <div class="row">
+            <div class="col-sm-12 important-text">
+                <p>Компания ООО «Колорэкспресс» - нестандартный подход к наружной рекламе.</p>
+                <p>Компания оказывает услуги по размещению рекламы международных и отечественных брендов на собственных рекламоносителях.</p>
+                <p>Уникальный опыт, полученный нами за 20 лет, позволяет реализовывать идеи любой сложности, обеспечив при этом Заказчика профессионализмом, качеством и гарантией.</p>
+                <p>Почему стоит выбрать нас?</p>
+                <p class="bold">Потому что нестандартно!</p>
+            </div>
+        </div>
+        <hr/>
+        <div class="row block-row">
+            <div class="col-sm-2 text-center">
+                <p><img src="../images/about-company/1.png"></p>
+                <p>Регистрируйтесь на сайте.</p>
+            </div>
+            <div class="col-sm-2 text-center">
+                <p><img src="../images/about-company/2.png"></p>
+                <p>Пользуйтесь удобным сервисом для поиска и бронирования рекламных конструкций.</p>
+            </div>
+            <div class="col-sm-2 text-center">
+                <p><img src="../images/about-company/3.png"></p>
+                <p>Следите за доступностью конструкций в режиме онлайн.</p>
+            </div>
+            <div class="col-sm-2 text-center">
+                <p><img src="../images/about-company/4.png"></p>
+                <p>Просматривайте историю Ваших заказов в личном кабинете.</p>
+            </div>
+            <div class="col-sm-2 text-center">
+                <p><img src="../images/about-company/5.png"></p>
+                <p>Получайте оповещение о доступности конструкции, если в данный момент она недоступна.</p>
+            </div>
+            <div class="col-sm-2 text-center">
+                <p><img src="../images/about-company/6.png"></p>
+                <p>Документы по каждой сделке в личном кабинете.</p>
+            </div>
+        </div>
+    </div>
 </div>
