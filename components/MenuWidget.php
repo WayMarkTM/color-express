f19e34d347030f213f10bb0c5e41f052b5adb9e7
Edward Chernyavsky <e.chernyavsky@itransition.com>
2017-04-10 18:50:03 +0300

    Menu widget.

---
+++ components/MenuWidget.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: e.chernyavsky
+ * Date: 10.04.2017
+ * Time: 18:05
+ */
+
+namespace app\components;
+
+
+use app\models\constants\SiteSettingKey;
+use app\models\entities\SiteSettings;
+use app\models\User;
+use Yii;
+use yii\base\Widget;
+
+class MenuWidget extends Widget
+{
+    public function run() {
+        $items = array();
+
+        if (Yii::$app->user->isGuest) {
+            $items = $this->getGuestMenu();
+        } else {
+            $user_id = Yii::$app->user->getId();
+            $user = User::findIdentity($user_id);
+            switch ($user->getRole()) {
+                case 'admin':
+                    $items = $this->getAdminMenu();
+                    break;
+                case 'client':
+                    $items = $this->getClientMenu();
+                    break;
+                case 'employee':
+                    $items = $this->getEmployeeMenu();
+                    break;
+            }
+        }
+
+
+        return $this->render('_menu', [
+            'items' => $items
+        ]);
+    }
+
+    private function getEmployeeMenu() {
+        return [
+            [
+                'label' => 'Управление клиентами',
+                'url' => ['clients/index']
+            ],
+            [
+                'label' => 'Управление конструкциями',
+                'url' => ['advertising-construction/index']
+            ],
+            [
+                'label' => 'Новые заявки на регистрацию',
+                'url' => ['registration-requests/index'],
+                'template' => '<a href="{url}">{label}</a>'.BadgeWidget::widget(['param' => BadgeWidget::$NEW_USER_COUNT]),
+            ],
+        ];
+    }
+
+    private function getClientMenu() {
+        return [
+            [
+                'label' => 'Оформить заказ',
+                'url' => ['advertising-construction/index']
+            ],
+            [
+                'label' => 'Корзина',
+                'url' => ['shopping-cart/index']
+            ],
+            [
+                'label' => 'Мои заказы',
+                'url' => ['orders/index']
+            ],
+            [
+                'label' => 'Документы',
+                'url' => ['site/index']
+            ]
+        ];
+    }
+
+    private function getAdminMenu() {
+        return [
+            [
+                'label' => 'Управление конструкциями',
+                'url' => ['advertising-construction/index']
+            ],
+            [
+                'label' => 'Управление размерами конструкций',
+                'url' => ['advertising-construction-size/index']
+            ],
+            [
+                'label' => 'Управление типами конструкций',
+                'url' => ['advertising-construction-type/index']
+            ],
+            [
+                'label' => 'Управление нашими клиентами',
+                'url' => ['our-client/index']
+            ],
+            [
+                'label' => 'Управление вакансиями',
+                'url' => ['vacancy/index']
+            ]
+        ];
+    }
+
+    private function getGuestMenu() {
+        return [
+            [
+                'label' => 'Каталог рекламных конструкций',
+                'url' => ['advertising-construction/index'],
+                'visible' => Yii::$app->user->isGuest,
+            ],
+            [
+                'label' => 'Преимущества',
+                'url' => ['site/advantages'],
+                'visible' => Yii::$app->user->isGuest,
+            ],
+            [
+                'label' => 'О компании',
+                'url' => ['site/about'],
+                'visible' => Yii::$app->user->isGuest,
+            ],
+            [
+                'label' => 'Наши клиенты',
+                'url' => ['site/clients'],
+                'visible' => Yii::$app->user->isGuest,
+            ],
+            [
+                'label' => 'Вакансии',
+                'url' => ['site/vacancies'],
+                'visible' => Yii::$app->user->isGuest,
+            ],
+            [
+                'label' => 'Контакты',
+                'url' => ['site/contact'],
+                'visible' => Yii::$app->user->isGuest,
+            ],
+            [
+                'label' => 'Презентация наших конструкций',
+                'url' => SiteSettings::findOne(SiteSettingKey::PRESENTATION_LINK)->value,
+                'visible' => Yii::$app->user->isGuest
+            ]
+        ];
+    }
+}
---
+++ models/constants/SiteSettingKey.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: e.chernyavsky
+ * Date: 10.04.2017
+ * Time: 18:20
+ */
+
+namespace app\models\constants;
+
+
+class SiteSettingKey
+{
+    const PRESENTATION_LINK = 1;
+}
