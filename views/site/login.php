4a0ea62043f62edca7524a33dfc8b143b121dd69
Yan Savitski <yan.savitski@gmail.com>
2017-05-14 16:28:18 +0300

    Login page fix

--- views/site/login.php
+++ views/site/login.php
@@ -15,8 +15,6 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
-
     <?php $form = ActiveForm::begin([
         'id' => 'login-form1',
         'layout' => 'horizontal',
@@ -24,6 +22,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
             'labelOptions' => ['class' => 'col-lg-1 control-label'],
         ],
+        'validationUrl' => \yii\helpers\Url::toRoute('admin/user/validate-login')
     ]); ?>
 
         <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
@@ -36,7 +35,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <?= Html::submitButton('Войти', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
             </div>
         </div>
 
