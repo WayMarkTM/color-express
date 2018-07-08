c9da3e07f7f6e9100aa73bcc1a1fab7b2cf5f1e9
Yan Savitski <yan.savitski@gmail.com>
2018-07-08 17:59:02 +0300

    Fixed Stock scale

--- components/views/_stock.php
+++ components/views/_stock.php
@@ -13,15 +13,13 @@ if ($model->content) {
 ?>
 
     <div class="stock-window">
-        <div class="stock-container">
-            <div class="content">
-                <a href="#close" title="Закрыть" class="close" onclick="$('.stock-window').hide();">X</a>
+        <div class="content">
+            <a href="#close" title="Закрыть" class="close" onclick="$('.stock-window').hide();">X</a>
 
-                <img src="/<?= $model->content; ?>">
-            </div>
-            <div class="watermark">
-                Сайт носит рекламно-информационный характер и не используется в качестве интернет-магазина, в том числе для торговли по образцам и с помощью курьера.
-            </div>
+            <img src="/<?= $model->content; ?>">
+        </div>
+        <div class="watermark">
+            Сайт носит рекламно-информационный характер и не используется в качестве интернет-магазина, в том числе для торговли по образцам и с помощью курьера.
         </div>
     </div>
 
