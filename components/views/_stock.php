dd71b246f2bf7c51e1dda8538082aa7d73ebc489
Yan Savitski <yan.savitski@gmail.com>
2018-07-24 11:29:12 +0300

    Fix stock window text

--- components/views/_stock.php
+++ components/views/_stock.php
@@ -13,13 +13,14 @@ if ($model->content) {
 ?>
 
     <div class="stock-window">
-        <div class="content">
-            <a href="#close" title="Закрыть" class="close" onclick="$('.stock-window').hide();">X</a>
-
-            <img src="/<?= $model->content; ?>">
-        </div>
-        <div class="watermark">
-            Сайт носит рекламно-информационный характер и не используется в качестве интернет-магазина, в том числе для торговли по образцам и с помощью курьера.
+        <div class="wrapper">
+            <div class="content">
+                <a href="#close" title="Закрыть" class="close" onclick="$('.stock-window').hide();">X</a>
+                <img src="/<?= $model->content; ?>">
+            </div>
+            <div class="watermark">
+                Сайт носит рекламно-информационный характер и не используется в качестве интернет-магазина, в том числе для торговли по образцам и с помощью курьера.
+            </div>
         </div>
     </div>
 
