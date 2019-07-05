ae232d06ba8ffdb26d0967a8d80fd3ee33726888
Alex Solodkiy <alexsolodkiy@mail.ru>
2019-07-05 11:34:25 +0300

    remastered front

--- front/index.js
+++ front/index.js
@@ -1,9 +1,3 @@
-
-
-  $(function(){    $('.single-slide').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });   });
-      
+$('.carousel').carousel({
+    interval:2000,
+})
